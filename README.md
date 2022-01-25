bc7b1a14f87f4074934bfa250cd68e844dc17b65
drewbrinkley <drewbrinkley24@gmail.com>
2022-01-25 15:00:50 -0500

    Update to describe other repo contents

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Dice Rolling Application
 
-This project will create a dice rolling application using Python.
+This project will create a dice rolling application using Python.  This project contains the following files:
+
+* The raw code for the app, dice.py
+* The full documentation describing the project, dice_roll_app.ipynb
--- dice.py
+++ dice.py
@@ -1,5 +1,28 @@
 # dice.py
 
+
+# create function 'parse_input()' with the argument set to the input string
+def parse_input(input_string):
+    # define docstring for the function
+    """Return 'input_string' as integer between 1 and 6.
+
+    Check if 'input_string' is an integer number between 1 and 6.
+    If so, return an integer with the same value. Othewise, tell
+    the user to enter a valid number and quit the program.
+    """
+    # create if-statement which will check that input is acceptable
+    if input_string.strip() in {"1", "2", "3", "4", "5", "6"}:
+        # return the input string as an integer, if it is validated
+        return int(input_string)
+    # create else-statement to handle invalid inputs
+    else:
+        # print string telling user to enter a valid number
+        print("Please enter a number from 1 to 6.")
+        # exits the app with 'SystemExit' exception and status code of 1 to show something went wrong
+        raise SystemExit(1)
+
+
+
 # ~~~ App's main code block ~~~
 # 1. Get and validate the user's input
 
